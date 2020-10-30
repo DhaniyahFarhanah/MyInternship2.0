96ea946db044666598beb2140d3e892a5d876342
Dhaniyah Farhanah <s1018598@connect.np.edu.sg>
2020-10-31 01:16:26 +0800

    Student, Intern and NPStaff UI done

---
+++ app/src/main/java/com/example/myinternship20/UIStaff.kt
@@ -0,0 +1,11 @@
+package com.example.myinternship20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UIStaff : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.uistaff)
+    }
+}
