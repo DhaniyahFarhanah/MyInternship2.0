9b58b0388201fbc91254a14a9bbb167b41f6a9ec
Dhaniyah Farhanah <s1018598@connect.np.edu.sg>
2020-11-01 19:08:39 +0800

    Finished basic layout. I dunno what master branch does so i'm confused

--- app/src/main/java/com/example/myinternship20/SetupIntern.kt
+++ app/src/main/java/com/example/myinternship20/SetupIntern.kt
@@ -4,11 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AbsSpinner
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.core.view.isVisible
+import com.example.myinternship20.databinding.ActivityMainBinding
+import com.example.myinternship20.databinding.SetupInternBinding
 import kotlinx.android.synthetic.main.loginsetup1.*
 import kotlinx.android.synthetic.main.setup_intern.*
 
@@ -18,21 +17,66 @@ class SetupIntern : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.setup_intern)
+        val binding = SetupInternBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val type = intent.getStringExtra("userType").toString()
-        val selectedSchool = findViewById<Spinner>(R.id.schoolSpinner)
-        val selectedCourse = findViewById<Spinner>(R.id.courseSpinner)
+        val arrayAdapter_school=ArrayAdapter.createFromResource(applicationContext,R.array.school_list,android.R.layout.simple_spinner_item)
 
-        setupUserType.text="$type setup"
-        setupUserEmail.hint="$type E-mail"
-        setupUserID.hint="$type ID"
+        binding.schoolSpinner.adapter=arrayAdapter_school
+        binding.setupUserType.text="$type setup"
+        binding.setupUserEmail.hint="$type E-mail"
+        binding.setupUserID.hint="$type ID"
 
         if(type=="Intern"){
             setupIntern.visibility=View.VISIBLE
         }
-        registerButton.setOnClickListener {
-            if(setupUserEmail.text.toString()==""||setupUserID.text.toString()==""||setupPassword.text.toString()==""){
+
+        binding.schoolSpinner.onItemSelectedListener=object:AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long
+            ) {
+                if(position==0){
+                    val arrayAdapter_course=ArrayAdapter.createFromResource(applicationContext,R.array.baCourse_list,android.R.layout.simple_spinner_item)
+                    binding.courseSpinner.adapter=arrayAdapter_course
+                }
+                if(position==1){
+                    val arrayAdapter_course=ArrayAdapter.createFromResource(applicationContext,R.array.deCourse_List,android.R.layout.simple_spinner_item)
+                    binding.courseSpinner.adapter=arrayAdapter_course
+                }
+                if(position==2){
+                    val arrayAdapter_course=ArrayAdapter.createFromResource(applicationContext,R.array.soeCourse_list,android.R.layout.simple_spinner_item)
+                    binding.courseSpinner.adapter=arrayAdapter_course
+                }
+                if(position==3){
+                    val arrayAdapter_course=ArrayAdapter.createFromResource(applicationContext,R.array.fmsCourse_list,android.R.layout.simple_spinner_item)
+                    binding.courseSpinner.adapter=arrayAdapter_course
+                }
+                if(position==4){
+                    val arrayAdapter_course=ArrayAdapter.createFromResource(applicationContext,R.array.hsCourse_list,android.R.layout.simple_spinner_item)
+                    binding.courseSpinner.adapter=arrayAdapter_course
+                }
+                if(position==5){
+                    val arrayAdapter_course=ArrayAdapter.createFromResource(applicationContext,R.array.hssCourse_list,android.R.layout.simple_spinner_item)
+                    binding.courseSpinner.adapter=arrayAdapter_course
+                }
+                if(position==6){
+                    val arrayAdapter_course=ArrayAdapter.createFromResource(applicationContext,R.array.ictCourse_List,android.R.layout.simple_spinner_item)
+                    binding.courseSpinner.adapter=arrayAdapter_course
+                }
+                if(position==7){
+                    val arrayAdapter_course=ArrayAdapter.createFromResource(applicationContext,R.array.lsctCourse_list,android.R.layout.simple_spinner_item)
+                    binding.courseSpinner.adapter=arrayAdapter_course
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+
+
+        binding.registerButton.setOnClickListener {
+            if(binding.setupUserEmail.text.toString()==""||binding.setupUserID.text.toString()==""||binding.setupPassword.text.toString()==""){
                 Toast.makeText(this,"Fill in empty blanks",Toast.LENGTH_SHORT).show()
             }
             else{//successful creation
@@ -41,7 +85,7 @@ class SetupIntern : AppCompatActivity() {
             }
         }
 
-        setupCancelButton.setOnClickListener{
+        binding.setupCancelButton.setOnClickListener{
             finish()
         }
 
