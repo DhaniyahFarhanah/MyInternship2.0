e7ab2dd12c8f0afe118906c931fd8a6b5caa618e
liptontea19 <daniel.lipws@gmail.com>
2021-01-22 15:23:37 +0800

    Added: StudentPersonalDataViewing.kt Students can finally view their own work

--- app/src/main/java/com/ngeeann/myinternship20/StudentPersonalDataViewing.kt
+++ app/src/main/java/com/ngeeann/myinternship20/StudentPersonalDataViewing.kt
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,38 +13,61 @@ import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.ngeeann.myinternship20.databinding.StudentPersonaldataViewingBinding
+import kotlinx.android.synthetic.main.student_personaldata_viewing.*
 import java.util.*
+import kotlin.collections.ArrayList
+
+/*
+Student Personal Attendance /////////Viewer
+1. Selected day chooses which day's attendance records they would like to view
+2. Send a query (querySchedule()) to the database, looking for the user's daily schedule for the selected day of the week and retrieves the values e.g. Tuesday has LNXSR, CCADC & BASRS
+    path: users/[userid]/Modules/[day of week] and then initializes values into a list for the day.
+3. queryAttendance() works by pathing to the specific date and then making use of an algorithm to find lessons that have the user's attendance listed in them
+   DB query path: db/attendance/[specific date]
+4. Displays all values in Step 2 lesson list, filling in with information from step 3 for lessons that were found and defaulting to "Absent" for those that were not
+5. User can pick another day from the same page, change the date sends a new query to the database and repeating steps 2, 3 & 4 with that date's information
+
+Step 3 algorithm works by attempting to query for a module found in the moduleNameGroupListArray. If it does find it, it will fill in the information into the 2 arrays, entryTimeListArray
+and statusLIstArray. If it doesn't, the program will assume that the student failed to show up for lessons and count them as absent.
+ */
 
 class StudentPersonalDataViewing : AppCompatActivity(), DatePickerDialog.OnDateSetListener
 {
     private lateinit var binding: StudentPersonaldataViewingBinding
+    private lateinit var userId: String
 
     private val cal = Calendar.getInstance()
     private val database = Firebase.database.reference
+    private val TAG = "StudentAttendance" //A log tag declaration for debugging usage. Search for "StudentAttendance" in Logcat to find log entries for the program
 
-    var chosenDay = 0
+    var chosenDate = 0
     var chosenMonth = 0
     var chosenYear = 0
 
-    //This group of arrays is for the schedule. The "Module" child in the student's account. Sample data
-    var moduleNameListArray = arrayListOf<String>("Module 1","Module 2","Module 3","Module 4","Module 5") //Array of modules from the "module" child from student's firebase data
-    var groupListArray = arrayListOf<String>("Class 1","Class 2","Class 3","Class 4","Class 5") //Array of groups from individual modules from student's firebase data
-    var startAtListArray = arrayOf<String>("startTime1","startTime2","startTime3","startTime4","startTime5") //Array of class start times from individual module from student's firebase data
-    var endAtListArray = arrayListOf<String>("endTime1","endTime2","endTime3","endTime4","endTime5") //Array of class end times from individual module child in student's firebase data
-    var locationListArray = arrayListOf<String>("location 1","location 2","hbl","location 3","hbl","hbl") //Array of class location from individual module child in student's firebase data
-
-    var entryTimeListArray = arrayListOf<String>("entryTime1","entryTime2","entryTime3","entryTime4","entryTime5") //Array of Student's log in time for attendance
-    var statusListArray = arrayListOf<String>("Present","Late","Absent","NA","NA") //Array of the status for the from the modules
+    //This group of arrays is for the schedule. Due to the RecyclerViews nature, it is required to place some default values in to prevent the application from taking a walk off a cliff.
+    var moduleNameGroupListArray = arrayListOf<String>("Module 1 - Class 1","Module 2 - Class 2","Module 3 - Class 3","Module 4 - Class 4","Module 5 - Class 5") //Array of modules and group hybrid names
+    var startAtListArray = arrayListOf<String>("startTime1","startTime2","startTime3","startTime4","startTime5") //Array of class start times
+    var endAtListArray = arrayListOf<String>("endTime1","endTime2","endTime3","endTime4","endTime5") //Array of class end times
+    var locationListArray = arrayListOf<String>("location 1","location 2","hbl","location 3","hbl","hbl") //Array of class locations
+    //^ These 4 array lists are used for querySchedule while the 2 below v are used for queryAttendance. All 6 are cleared out during the function set call
+    var entryTimeListArray = arrayListOf<String>("entryTime1","entryTime2","entryTime3","entryTime4","entryTime5") //Array of Student's attendance times
+    var statusListArray = arrayListOf<String>("Present","Late","Absent","NA","NA") //Array of the student's lesson attendance statuses, Present, Late, MC and Absent.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        userId = intent.getStringExtra("userId").toString()
         binding = StudentPersonaldataViewingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         getDateCalendar() //gets date of today
+        querySchedule(userId, changeDayToString(cal.get(Calendar.DAY_OF_WEEK)))
 
         binding.studentAttendanceRecyclerView.layoutManager = LinearLayoutManager(this) //vertical layout for recycler view
 
@@ -53,20 +77,28 @@ class StudentPersonalDataViewing : AppCompatActivity(), DatePickerDialog.OnDateS
 
         binding.dateTextView.setOnClickListener { //
             pickDate()
+            querySchedule(userId,changeDayToString(cal.get(Calendar.DAY_OF_WEEK)))
         }
 
-        binding.studentAttendanceRecyclerView.adapter=StudentAttendanceRecyclerAdapter() //resets recycler data shown
+        binding.studentAttendanceRecyclerView.adapter = StudentAttendanceRecyclerAdapter() //resets recycler data shown
 
         binding.dayDisplayTextView.setOnClickListener { //allows user to open date picker
             pickDate()
+            querySchedule(userId,changeDayToString(cal.get(Calendar.DAY_OF_WEEK)))
         }
 
         binding.dateNextButton.setOnClickListener { //goes to the next day by 1
-            nextDay()
+            //nextDay()
+            cal.add(Calendar.DATE,1)
+            getDateCalendar()
+            querySchedule(userId,changeDayToString(cal.get(Calendar.DAY_OF_WEEK)))
         }
 
         binding.datePreviousButton.setOnClickListener { //goes to the previous day by 1
-            previousDay()
+            //previousDay()
+            cal.add(Calendar.DATE,-1)
+            getDateCalendar()
+            querySchedule(userId,changeDayToString(cal.get(Calendar.DAY_OF_WEEK)))
         }
 
     } //end of activity code
@@ -84,26 +116,22 @@ class StudentPersonalDataViewing : AppCompatActivity(), DatePickerDialog.OnDateS
     }
 
     private fun getDateCalendar() { //gets the date and sets the date into text View
-
-        chosenDay = cal.get(Calendar.DAY_OF_MONTH)
+        chosenDate = cal.get(Calendar.DAY_OF_MONTH)
         chosenMonth = cal.get(Calendar.MONTH)
         chosenYear = cal.get(Calendar.YEAR)
 
-        binding.dateTextView.text = "$chosenDay ${changeMonthToString(chosenMonth)} $chosenYear"
+        binding.dateTextView.text = "$chosenDate ${changeMonthToString(chosenMonth)} $chosenYear"
         binding.dayDisplayTextView.text = "${changeDayToString(cal.get(Calendar.DAY_OF_WEEK))}"
-
     }
 
     private fun pickDate() { //opens date picker
-
         getDateCalendar()
-        DatePickerDialog(this,this, chosenYear, chosenMonth, chosenDay).show()
-
+        DatePickerDialog(this,this, chosenYear, chosenMonth, chosenDate).show()
     }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) { //on date picker change date
 
-        chosenDay = dayOfMonth
+        chosenDate = dayOfMonth
         chosenMonth = month
         chosenYear = year
 
@@ -111,11 +139,11 @@ class StudentPersonalDataViewing : AppCompatActivity(), DatePickerDialog.OnDateS
         cal.set(Calendar.MONTH,month)
         cal.set(Calendar.DAY_OF_MONTH,dayOfMonth)
 
-        binding.dateTextView.text = "$chosenDay ${changeMonthToString(chosenMonth)} $chosenYear"
+        binding.dateTextView.text = "$chosenDate ${changeMonthToString(chosenMonth)} $chosenYear"
         binding.dayDisplayTextView.text = "${changeDayToString(cal.get(Calendar.DAY_OF_WEEK))}"
-
     }
 
+    /*
     private fun nextDay() { //sets calendar to next day
         cal.add(Calendar.DATE,1)
         getDateCalendar()
@@ -125,6 +153,7 @@ class StudentPersonalDataViewing : AppCompatActivity(), DatePickerDialog.OnDateS
         cal.add(Calendar.DATE,-1)
         getDateCalendar()
     }
+    */
 
     inner class StudentAttendanceRecyclerAdapter: RecyclerView.Adapter<StudentAttendanceRecyclerAdapter.ViewHolder>() { //the adapter for the recycler view
 
@@ -134,17 +163,19 @@ class StudentPersonalDataViewing : AppCompatActivity(), DatePickerDialog.OnDateS
             return ViewHolder(v)
         }
 
-        /* places the data into the respective item views for student_attendanceviewing_card.xml
+        /*
+        places the data into the respective item views for student_attendanceviewing_card.xml
                 moduleGroupLabel<TextView> --> "64CCADC-PB13"
                 classTime<TextView> --> "13:00 - 14:00"
                 status<TextView> --> "Absent"
                 location<TextView> --> "Blk 5 #03-02"
                 time<TextView> --> "13:05" (entry time)
-                cardView<constraintLayout> --> It's the layout where all the details are in. Used to change the color.*/
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+                cardView<constraintLayout> --> It's the layout where all the details are in. Used to change the color.
+        */
 
-            holder.moduleGroupLabel.text = "${moduleNameListArray[position]}-${groupListArray[position]}"
-            holder.classTime.text = "${startAtListArray[position]} - ${endAtListArray[position]}"
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.moduleGroupLabel.text = "${moduleNameGroupListArray[position]}"
+            holder.classTime.text = "${startAtListArray[position]}-${endAtListArray[position]}"
             holder.status.text = "${statusListArray[position]}"
             holder.location.text ="${locationListArray[position]}"
 
@@ -164,15 +195,13 @@ class StudentPersonalDataViewing : AppCompatActivity(), DatePickerDialog.OnDateS
                     holder.cardView.setBackgroundColor(Color.parseColor("#26B357")) //green
                 }
             }
-
         }
 
-        override fun getItemCount(): Int {
-            return groupListArray.size //this will give the total number of items in array. So 5 items will give int of 4
+        override fun getItemCount(): Int { //size always takes from student's daily schedule, never from attendance logs
+            return moduleNameGroupListArray.size //this will give the total number of items in array. So 5 items will give int of 4
         }
 
-        //initializing of what values will be placed in which textView.
-        inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) { //initializing of what values will be placed in which textView.
 
             var moduleGroupLabel: TextView = itemView.findViewById(R.id.moduleGroup_card)
             var classTime: TextView = itemView.findViewById(R.id.classTime_card)
@@ -181,8 +210,87 @@ class StudentPersonalDataViewing : AppCompatActivity(), DatePickerDialog.OnDateS
             var time: TextView = itemView.findViewById(R.id.entryTime_card)
             var cardView: ConstraintLayout = itemView.findViewById(R.id.studentAttendanceCardView)
 
-        } //
+        }
 
     } //end of adapter
 
+    fun findCommonElements (arr1: ArrayList<String>, arr2: ArrayList<String>): Array<String?> { //finds common elements in both arraylists, this is normal and acceptable
+        // returns an array with the common values
+        val hash1 = HashSet(arr1)
+        val hash2 = HashSet(arr2)
+        val finArray = arrayOfNulls<String>(hash1.size)
+
+        hash1.retainAll(hash2) //checks elements in array2, keep matching elements
+        hash1.toArray(finArray) //converts hashmap hash1 to a String array
+        return finArray //returns array
+    }
+
+    private fun conDateToString(): String{ //Format of YYYY-MM-DD
+        val month = if ((chosenMonth + 1) < 10){
+            "0${chosenMonth + 1}"
+        }
+        else {
+            "${chosenMonth + 1}"
+        }
+        val date = if (chosenDate < 10){
+            "0$chosenDate"
+        }
+        else {
+            chosenDate
+        }
+        return "$chosenYear-$month-$date"
+    }
+
+    private fun querySchedule (userId: String, dayOfWeek: String) {
+        Log.d(TAG, "Starting querySchedule function. Day of week is $dayOfWeek")
+        val path = database.child("users/$userId/Modules/$dayOfWeek").orderByChild("StartAt")
+        path.addListenerForSingleValueEvent( object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                moduleNameGroupListArray.clear()
+                locationListArray.clear()
+                startAtListArray.clear()
+                endAtListArray.clear()
+
+                for (modSnapshot in snapshot.children) {
+                    moduleNameGroupListArray.add(modSnapshot.key.toString()) //e.g. 64CCADC-LB12 [Module-Group]
+                    Log.i(TAG,"Module: ${modSnapshot.key.toString()}")
+                    locationListArray.add(modSnapshot.child("Location").value.toString())
+                    startAtListArray.add(modSnapshot.child("StartAt").value.toString())
+                    endAtListArray.add(modSnapshot.child("EndAt").value.toString())
+                }
+                queryAttendance(userId, conDateToString(), moduleNameGroupListArray) //as the required information is only supplied at the end of querySchedule, the second function is called at the end
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Schedule Query Failed")
+            }
+        })
+    }
+
+
+    private fun queryAttendance (userId: String, date: String, modListArray: ArrayList<String>) {
+        val path = database.child("attendance/$date").orderByChild("$userId/EntryTime")
+        val entryNameArrayList = arrayListOf<String>()
+        path.addListenerForSingleValueEvent( object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) { //initializes data into array, checks for matches with moduleNameListArray, detailListArray gets filled with information for each one
+                entryTimeListArray.clear()
+                statusListArray.clear()
+                for (i in modListArray) { //these 3 lists will be added to if the student was present for class
+                    if (snapshot.child("$i/$userId").exists()) {
+                        Log.i(TAG, "Lesson $i found")
+                        entryTimeListArray.add(snapshot.child("$i/$userId/EntryTime").value.toString())
+                        statusListArray.add(snapshot.child("$i/$userId/Status").value.toString())
+                    } else {
+                        entryTimeListArray.add("")
+                        statusListArray.add("Absent")
+                    }
+                }
+                studentAttendanceRecyclerView.adapter?.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Attendance Query Failed")
+            }
+        })
+    }
 }
